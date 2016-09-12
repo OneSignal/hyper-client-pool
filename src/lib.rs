162db8dac608c7e5cbdc5796c5dad52f08496b41
Joe Wilm <joe@jwilm.com>
2016-09-12 15:36:14 -0700

    Implement a pool of hyper::Client
    Provides a pool of hyper::Client workers for maximizing throughput on
    multicore systems. A pool is created by passing it some configuration
    including the number of clients to pool and how many sockets each client
    should try and use.
    
    Clients are stored in a list. Each request that is added starts at the
    beginning of the list and tries to run on the first client that has
    room. This has the effect of preferring loading up one event loop on a
    single thread instead of spreading evenly across all workers.
    
        let mut config = Config::default();
        config.workers = 8;
        config.max_sockets = 10_000;
    
        let pool = Pool::new(&mut config);
        let (tx, rx) = mpsc::channel();
    
        for _ in 0..5 {
            pool.request(
                Url::parse("https://www.httpbin.org").unwrap(),
                Transaction::new(
                    tx.clone(),
                    Method::Get,
                    Default::default(),
                    None
                )
            ).expect("request ok");
        }
    
        assert_eq!(pool.clients[0].active_transactions(), 5);
    
        let mut received = 0;
        while let Ok(result) = rx.recv() {
            println!("got result: {:?}\n\n", result);
    
            received += 1;
            if received == 5 {
                break;
            }
        }

---
+++ src/lib.rs
@@ -0,0 +1,309 @@
+#![feature(arc_counts)]
+#![feature(reflect_marker)]
+
+extern crate hyper;
+#[macro_use] extern crate log;
+
+use std::io;
+use std::time::Duration;
+use std::fmt;
+use std::mem;
+
+use hyper::client::{Request, Handler, Response, DefaultTransport};
+use hyper::{Next, Encoder, Decoder};
+use hyper::method::Method;
+use hyper::Headers;
+
+pub mod pool;
+
+pub trait Deliverable : Send + 'static {
+    fn complete(self, result: DeliveryResult);
+}
+
+pub use pool::Pool;
+
+#[derive(Debug)]
+pub struct Transaction<D>
+    where D: Deliverable
+{
+    state: State,
+    request_body: Option<Vec<u8>>,
+    written: usize,
+    response_body: Vec<u8>,
+    response: Option<Response>,
+    deliverable: Option<D>,
+}
+
+pub enum DeliveryResult {
+    Response {
+        inner: Response,
+        body: Vec<u8>
+    },
+
+    IoError {
+        error: io::Error,
+        response: Option<Response>,
+    },
+
+    HyperError {
+        error: hyper::Error,
+        response: Option<Response>,
+    }
+}
+
+impl fmt::Debug for DeliveryResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            DeliveryResult::Response { ref inner, ref body } => {
+                f.debug_struct("DeliveryResult::Response")
+                    .field("inner", inner)
+                    .field("body", &::std::str::from_utf8(&body[..]))
+                    .finish()
+            },
+            DeliveryResult::IoError { ref error, ref response } => {
+                f.debug_struct("DeliveryResult::IoError")
+                    .field("error", error)
+                    .field("response", response)
+                    .finish()
+            },
+            DeliveryResult::HyperError { ref error, ref response } => {
+                f.debug_struct("DeliveryResult::HyperError")
+                    .field("error", error)
+                    .field("response", response)
+                    .finish()
+            },
+        }
+    }
+}
+
+impl<D> Transaction<D>
+    where D: Deliverable
+{
+    pub fn new(deliverable: D,
+               method: Method,
+               headers: Headers,
+               body: Option<Vec<u8>>)
+        -> Transaction<D>
+    {
+        Transaction {
+            state: New { method: method, headers: headers },
+            request_body: body,
+            written: 0,
+            response_body: Vec::new(),
+            response: None,
+            deliverable: Some(deliverable)
+        }
+    }
+}
+
+impl<D> Drop for Transaction<D>
+    where D: Deliverable
+{
+    fn drop(&mut self) {
+        let state = mem::replace(&mut self.state, Sending); // don't care what state is anymore
+        let response = self.response.take();
+        let response_body = mem::replace(&mut self.response_body, Vec::new());
+        let deliverable = self.deliverable.take().unwrap();
+
+        let response = match state {
+            New { .. } | Sending => return,
+            IoError { error } => {
+                DeliveryResult::IoError { error: error, response: response }
+            },
+            HyperError { error } => {
+                DeliveryResult::HyperError { error: error, response: response }
+            },
+            Receiving => {
+                DeliveryResult::Response { inner: response.unwrap(), body: response_body }
+            },
+        };
+
+        deliverable.complete(response);
+    }
+}
+
+#[derive(Debug)]
+enum State {
+    /// New, unsent request
+    ///
+    /// `initfn` here is working around a deficit in the hyper API where only
+    /// one header can be set at a time instead of a whole replacement of the
+    /// headers.
+    // need way to provide r
+    New { method: Method, headers: Headers },
+
+    /// Making request without body
+    Sending,
+
+    /// Receiving response
+    Receiving,
+
+    /// Got an io::Error and cannot complete
+    IoError { error: io::Error },
+
+    /// Got a hyper error and cannot be complete
+    HyperError { error: hyper::Error },
+}
+
+use self::State::*;
+
+#[inline]
+fn read() -> Next {
+    Next::read().timeout(Duration::from_secs(10))
+}
+
+impl<D> Handler<DefaultTransport> for Transaction<D>
+    where D: Deliverable,
+{
+    fn on_request(&mut self, req: &mut Request) -> Next {
+        let state = ::std::mem::replace(&mut self.state, State::Sending);
+        match state {
+            New { method, headers } => {
+
+                // Set provided headers on the hyper request.
+                //
+                // Replacing the default headers isn't exactly supported out of
+                // the box. One workaround is using extend() which requires
+                // downcasting and clones, or there's this version which
+                // requires grabbing the only header originally set, adding it
+                // to ours, and setting them as the request headers.
+                let mut original = ::std::mem::replace(req.headers_mut(), headers);
+                let host = original.remove::<hyper::header::Host>();
+                if let Some(host) = host {
+                    req.headers_mut().set(host);
+                }
+
+                // Set the request method
+                req.set_method(method);
+
+                // Request write interest if there's a request body.
+                if self.request_body.is_some() {
+                    Next::write()
+                } else {
+                    read()
+                }
+            },
+            Sending => Next::write(),
+            _ => Next::read(),
+        }
+    }
+
+    fn on_request_writable(&mut self, encoder: &mut Encoder<DefaultTransport>) -> Next {
+        match (&self.state, self.request_body.as_ref()) {
+            (&Sending, Some(ref body)) => {
+                match encoder.write(&body[self.written..]) {
+                    Ok(n) => {
+                        self.written += n;
+                    },
+                    Err(err) => match err.kind() {
+                        io::ErrorKind::WouldBlock => (),
+                        _ => {
+                            // Unrecoverable?
+                            self.state = IoError { error: err };
+                            return Next::end()
+                        }
+                    }
+                }
+
+                if self.written != body.len() {
+                    return Next::write();
+                }
+            },
+            _ => ()
+        }
+
+        encoder.close();
+        read()
+    }
+
+    fn on_response(&mut self, res: Response) -> Next {
+        self.response = Some(res);
+        self.state = Receiving;
+
+        // Assume there is a body to read; should just read 0 if not.
+        read()
+    }
+
+    fn on_response_readable(&mut self, decoder: &mut Decoder<DefaultTransport>) -> Next {
+        let mut buf: [u8; 2048] = unsafe { ::std::mem::uninitialized() };
+
+        loop {
+            match decoder.read(&mut buf[..]) {
+                // End of body
+                Ok(0) => {
+                    return Next::end()
+                }
+
+                // Continue reading
+                Ok(bytes) => {
+                    self.response_body.extend_from_slice(&buf[..bytes]);
+                },
+
+                // io::Error
+                Err(err) => match err.kind() {
+                    io::ErrorKind::WouldBlock => {
+                        return read()
+                    },
+                    _ => {
+                        self.state = IoError { error: err };
+                        return Next::end()
+                    }
+                }
+            }
+        }
+    }
+
+    fn on_error(&mut self, err: hyper::Error) -> Next {
+        self.state = HyperError { error: err };
+        Next::remove()
+    }
+}
+
+impl Deliverable for ::std::sync::mpsc::Sender<DeliveryResult> {
+    fn complete(self, result: DeliveryResult) {
+        let _ = self.send(result)
+            .map_err(|e| println!("failed to emit DeliveryResult {:?}", e.0));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate env_logger;
+
+    use std::time::Duration;
+    use std::sync::mpsc;
+
+    use hyper::client::Client;
+    use hyper::method::Method;
+    use hyper::{Headers, Url};
+
+    use super::{DeliveryResult, Transaction};
+
+    #[test]
+    fn it_works() {
+        let _ = env_logger::init();
+
+        let (tx, rx) = mpsc::channel();
+        let client = Client::<Transaction<mpsc::Sender<DeliveryResult>>>::configure()
+            .keep_alive(true)
+            .keep_alive_timeout(Some(Duration::from_secs(60)))
+            .max_sockets(30_000)
+            .connect_timeout(Duration::from_secs(10))
+            .build()
+            .unwrap();
+
+        let url = Url::parse("https://www.httpbin.org").unwrap();
+
+        let transaction = Transaction::new(
+            tx,
+            Method::Get,
+            Headers::default(),
+            None);
+
+
+        client.request(url, transaction).unwrap();
+
+        rx.recv().unwrap();
+        client.close();
+    }
+}
---
+++ src/pool.rs
@@ -0,0 +1,352 @@
+//! HTTP Client Worker Pool
+//!
+//! This module provides a simple API wrapping a pool of HTTP clients
+use std::time::Duration;
+use std::cmp;
+use std::sync::Arc;
+use std::fmt;
+use std::marker::Reflect;
+
+use hyper::client::{Request, Handler, Response, DefaultTransport};
+use hyper::{Next, Encoder, Decoder};
+use hyper;
+use hyper::Url;
+
+use super::{Transaction, Deliverable};
+
+/// This is essentially an AtomicUsize that is clonable and whose count is based
+/// on the number of copies. The count is automaticaly updated on drop.
+#[derive(Clone)]
+struct Count(Arc<u8>);
+
+impl Count {
+    /// Get the count
+    ///
+    /// This method is inherently racey. Assume the count will have changed once
+    /// the value is observed.
+    #[inline]
+    pub fn get(&self) -> usize {
+        Arc::strong_count(&self.0)
+    }
+
+    /// Create a new RAII counter
+    pub fn new() -> Count {
+        // Note the value in the Arc doesn't matter since we rely on the Arc's
+        // strong count to provide a value.
+        Count(Arc::new(0))
+    }
+}
+
+/// Wraps a hyper client with additional information/APIs like active transaction count
+struct Client<D: Deliverable> {
+    /// Number of transactions running on this client
+    transactions: Count,
+
+    /// The hyper client being wrapped
+    inner: hyper::Client<PooledTransaction<D>>,
+
+    /// Maximum active transactions,
+    max_parallel_transactions: usize,
+}
+
+impl<D: Deliverable> Client<D> {
+    /// Create a new client
+    pub fn new(config: &Config) -> Client<D> {
+        let hyper_client = hyper::Client::<PooledTransaction<D>>::configure()
+            .keep_alive(true)
+            .keep_alive_timeout(Some(config.keep_alive_timeout))
+            .max_sockets(config.max_sockets + 100)
+            .connect_timeout(config.connection_timeout)
+            .build()
+            .unwrap(); // TODO
+
+        Client {
+            inner: hyper_client,
+            transactions: Count::new(),
+            max_parallel_transactions: config.max_sockets,
+        }
+    }
+
+    #[inline]
+    pub fn active_transactions(&self) -> usize {
+        self.transactions.get() - 1
+    }
+
+    #[inline]
+    pub fn is_full(&self) -> bool {
+        let active = self.active_transactions();
+        println!("active_transactions: {}, max: {}", active, self.max_parallel_transactions);
+        self.active_transactions() >= self.max_parallel_transactions
+    }
+
+    #[inline]
+    fn pooled(&self, transaction: Transaction<D>) -> PooledTransaction<D> {
+        PooledTransaction {
+            inner: transaction,
+            _count: self.transactions.clone(),
+        }
+    }
+
+    pub fn request(
+        &self,
+        url: Url,
+        transaction: Transaction<D>
+    ) -> Result<(), Option<(Url, Transaction<D>)>> {
+        self.inner
+            .request(url, self.pooled(transaction))
+            .map_err(|err| err.recover().map(|(url, trans)| (url, trans.into_transaction())))
+    }
+}
+
+/// Wrapper for transaction data
+///
+/// Used as an RAII tracker for connection count per client
+struct PooledTransaction<D: Deliverable> {
+    inner: Transaction<D>,
+
+    /// Number of active connections on current client
+    _count: Count,
+}
+
+impl<D: Deliverable> Handler<DefaultTransport> for PooledTransaction<D> {
+    #[inline]
+    fn on_request(&mut self, req: &mut Request) -> Next {
+        self.inner.on_request(req)
+    }
+
+    #[inline]
+    fn on_request_writable(&mut self, encoder: &mut Encoder<DefaultTransport>) -> Next {
+        self.inner.on_request_writable(encoder)
+    }
+
+    #[inline]
+    fn on_response(&mut self, res: Response) -> Next {
+        self.inner.on_response(res)
+    }
+
+    #[inline]
+    fn on_response_readable(&mut self, decoder: &mut Decoder<DefaultTransport>) -> Next {
+        self.inner.on_response_readable(decoder)
+    }
+
+    #[inline]
+    fn on_error(&mut self, err: hyper::Error) -> Next {
+        self.inner.on_error(err)
+    }
+}
+
+impl<D: Deliverable> PooledTransaction<D> {
+    pub fn into_transaction(self) -> Transaction<D> {
+        self.inner
+    }
+}
+
+/// Pool of HTTP clients
+///
+/// Manages a set of `hyper::Client` for maximizing throughput while presenting
+/// a `request` API similar to using a `hyper::Client` directly. The number of
+/// active transactions running on each client is tracked so that max sockets
+/// may be respected. When all clients are full, backpressure is provided in the
+/// form of an Error variant saying "busy; try again later".
+pub struct Pool<D: Deliverable> {
+    clients: Vec<Client<D>>,
+    _max_parallel_transactions: usize,
+}
+
+pub struct Config {
+    /// How long to keep a connection alive before timing out
+    keep_alive_timeout: Duration,
+
+    /// Connection timeout (in seconds)
+    connection_timeout: Duration,
+
+    /// Maximum sockets per worker
+    max_sockets: usize,
+
+    /// Number of workers in the pool
+    workers: usize,
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            keep_alive_timeout: Duration::from_secs(60),
+            connection_timeout: Duration::from_secs(10),
+            max_sockets: 10_000,
+            workers: 8,
+        }
+    }
+}
+
+pub enum Error<D: Deliverable> {
+    /// The pool is processing the maximum number of transactions
+    Full {
+        url: Url,
+        transaction: Transaction<D>,
+    },
+
+    /// It seems that hyper client error won't always return the stuff passed
+    /// into it. Not sure what this means, but we don't have the transaction and
+    /// url any longer :(.
+    LostToEther
+}
+
+impl<D: Deliverable> fmt::Debug for Error<D> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Full { .. } => {
+                write!(f, "Full {{ .. }}")
+            },
+            Error::LostToEther => {
+                f.debug_struct("LostToEther").finish()
+            }
+        }
+    }
+}
+
+impl<D: Deliverable + Reflect> ::std::error::Error for Error<D> {
+    fn cause(&self) -> Option<&::std::error::Error> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        match *self {
+            Error::Full { .. } => "client pool at max capacity",
+            Error::LostToEther => "passed to hyper client but something bad happened",
+        }
+    }
+}
+
+impl<D: Deliverable + Reflect> ::std::fmt::Display for Error<D> {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "{}", ::std::error::Error::description(self))
+    }
+}
+
+impl<D: Deliverable> Pool<D> {
+    /// Creat a new pool according to config
+    pub fn new(config: &mut Config) -> Pool<D> {
+        // Make sure config.workers is a reasonable value
+        let num_workers = cmp::max(1, config.workers);
+        config.workers = num_workers;
+
+        // Ditto for max_sockets
+        let max_sockets = cmp::max(1, config.max_sockets);
+        config.max_sockets = max_sockets;
+
+        // Maximum number of active transactions would be using each socket on
+        // each worker.
+        let max_parallel_transactions = max_sockets * num_workers;
+
+        let clients = (0..num_workers)
+            .map(|_| Client::new(&config))
+            .collect();
+
+        Pool {
+            clients: clients,
+            _max_parallel_transactions: max_parallel_transactions
+        }
+    }
+
+    /// Start or queue a request
+    ///
+    /// The request will be started immediately assuming one of the clients in
+    /// this pool is not at max_sockets.
+    pub fn request(
+        &self,
+        url: Url,
+        transaction: Transaction<D>
+    ) -> Result<(), Error<D>> {
+        // This strategy will favor clients earlier in the list over clients
+        // later in the list.  There's pros/cons to this vs load balancing, but
+        // my current justification for this approach is 1) it's easy to write,
+        // and 2) results in fewer context switches, and 3) event loops are good
+        // at lots of parallel activity.
+        for client in &self.clients {
+            if !client.is_full() {
+                try!(client
+                    .request(url, transaction)
+                    .map_err(|err| {
+                        err.map(|(url, transaction)| {
+                            Error::Full { url: url, transaction: transaction }
+                        }).unwrap_or(Error::LostToEther)
+                    }));
+                return Ok(());
+            }
+        }
+
+        Err(Error::Full {
+            url: url,
+            transaction: transaction
+        })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::sync::mpsc;
+
+    use hyper::Url;
+    use hyper::method::Method;
+
+    use super::{Config, Pool, Error};
+    use ::{Transaction};
+
+    #[test]
+    fn lots_of_get_single_worker() {
+        let mut config = Config::default();
+        config.workers = 1;
+
+        let pool = Pool::new(&mut config);
+        let (tx, rx) = mpsc::channel();
+
+        for _ in 0..5 {
+            pool.request(
+                Url::parse("https://www.httpbin.org").unwrap(),
+                Transaction::new(tx.clone(), Method::Get, Default::default(), None)
+            ).expect("request ok");
+        }
+
+        assert_eq!(pool.clients[0].active_transactions(), 5);
+
+        let mut received = 0;
+        while let Ok(result) = rx.recv() {
+            println!("got result: {:?}\n\n", result);
+
+            received += 1;
+            if received == 5 {
+                break;
+            }
+        }
+    }
+
+    #[test]
+    fn full_error() {
+        let mut config = Config::default();
+        config.workers = 1;
+        config.max_sockets = 1;
+
+        let pool = Pool::new(&mut config);
+        let (tx, rx) = mpsc::channel();
+
+        // Start first request
+        pool.request(
+            Url::parse("https://www.httpbin.org").unwrap(),
+            Transaction::new(tx.clone(), Method::Get, Default::default(), None)
+        ).expect("request ok");
+
+        // Should be counted
+        assert_eq!(pool.clients[0].active_transactions(), 1);
+
+        match pool.request(
+            Url::parse("https://www.httpbin.org").unwrap(),
+            Transaction::new(tx.clone(), Method::Get, Default::default(), None)
+        ) {
+            Err(Error::Full { .. }) => (),
+            res => panic!("got expected Error::Full; got {:?}", res),
+        }
+
+        rx.recv().unwrap();
+    }
+}
